f99a3f6055920f857f7280fb0ae6f41e379f1851
Anant7393 <anantmishra7393@gmail.com>
2022-08-26 16:15:19 +0530

    a

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,3 @@
 //let url = 'https://onerecruit.herokuapp.com';
-let url = 'https://cryptic-badlands-27446.herokuapp.com/ ';
+let url = 'https://cryptic-badlands-27446.herokuapp.com';
 module.exports = { url };
